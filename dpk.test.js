d4c87d4f9a94f01a240b3139e3577a57e3d503fe
Nuel <ibk.com@gmail.com>
2023-03-29 01:10:36 +0100

    refctored completed

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,31 @@
 const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
-  });
+	it("Returns the literal '0' when given no input", () => {
+		const trivialKey = deterministicPartitionKey();
+		expect(trivialKey).toBe("0");
+	});
+
+	it("should return the trivial partition key when called with no event", () => {
+		expect(deterministicPartitionKey()).toEqual("0");
+	});
+
+	it("should return the event's partition key when it exists", () => {
+		const event = { partitionKey: "some-partition-key" };
+		expect(deterministicPartitionKey(event)).toEqual("some-partition-key");
+	});
+
+	it("should hash the event when it doesn't have a partition key", () => {
+		const event = { foo: "bar" };
+		const hash = crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+		expect(deterministicPartitionKey(event)).toEqual(hash);
+	});
+
+	it("should hash the candidate when its length is greater than 256", () => {
+		const event = { foo: "bar" };
+		const longCandidate = "x".repeat(257);
+		const hash = crypto.createHash("sha3-512").update(longCandidate).digest("hex");
+		expect(deterministicPartitionKey({ ...event, partitionKey: longCandidate })).toEqual(hash);
+	});
 });
